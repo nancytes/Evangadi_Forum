2be2e4bcf69f2ea5b26772033e6c43c13b614e63
Nancy Tesfaye <55075437+nancytes@users.noreply.github.com>
2024-08-05 10:42:37 +0300

    Update answer.js

--- Back_End/controller/answer.js
+++ Back_End/controller/answer.js
@@ -36,5 +36,19 @@ async function createAnswer(req, res) {
 			.json({ msg: "Something went wrong" });
 	}
 }
-
-module.exports = { createAnswer };
+async function GetAnswersByQuestionId(req,res){
+    const questionId= req.params.questionId;
+    try {
+        const query="SELECT q.*,u.username FROM answers q INNER JOIN users u ON q.userid=u.userid WHERE questionid = ?";
+        const result =await dbConnectionPromise.query(query,[questionId])
+        if (result.length > 0) {
+            return res.status(StatusCodes.OK).json(result[0]);
+        } else {
+            return res.status(StatusCodes.NOT_FOUND).json({ msg: "Question not found" });
+        }
+    } catch (error) {
+        console.error("Error fetching question detail:", error);
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: "Something went wrong" });
+    }
+}
+module.exports = { createAnswer, GetAnswersByQuestionId };
