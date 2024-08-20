2b8786c47191085c30e5d47792f08512d52b55b9
Nancy Tesfaye <nancy23tes@gmail.com>
2024-08-20 18:37:50 +0300

    update

--- Front-End/evangadi-forum/src/Components/axios.js
+++ Front-End/evangadi-forum/src/Components/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 const axiosBase = axios.create({
-    baseURL:'http://localhost:3000/api'
+    // baseURL:'http://localhost:3000/api'
+    baseURL: 'https://forum-backend-2-am7z.onrender.com'
 })
 export default axiosBase
